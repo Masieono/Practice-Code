2cd09ef9c49512bfbe4b7413957495b6f6a8b262
Masieono <masonvoskamp@gmail.com>
2023-07-03 22:33:52 -0500

    some lessons

--- C++/020_Console_Calculator.cpp
+++ C++/020_Console_Calculator.cpp
@@ -1,14 +1,53 @@
 #include <iostream>
 
 /*
-
+    A simple calculator terminal application that 
+    allows for the input of an operator, followed 
+    by two numbers. The operator is used on the 
+    variables in a switch case that prints the 
+    result to the console.
 */
 
 using namespace std;
 
 int main () {
 
+    char op;
+    double num1;
+    double num2;
+    double result;
 
+    cout << "LA CALCULADORA \n";
+    cout << "Enter either + - * or / \n";
+    cin >> op;
+
+    cout << "Enter first number: \n";
+    cin >> num1;
+
+    cout << "Enter second number: \n";
+    cin >> num2;
+
+    switch(op) {
+        case '+':
+            result = num1 + num2;
+            cout << "sum = " << result << "\n";
+            break;
+        case '-':
+            result = num1 - num2;
+            cout << "difference = " << result << "\n";
+            break;
+        case '*':
+            result = num1 * num2;
+            cout << "product = " << result << "\n";
+            break;
+        case '/':
+            result = num1 / num2;
+            cout << "result = " << result << "\n";
+            break;
+        default:
+            cout << "Sorry, not a valid operator";
+            break;
+    }
 
     return 0;
 }
