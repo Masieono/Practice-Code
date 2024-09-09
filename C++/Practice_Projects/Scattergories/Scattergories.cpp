0e900cde18de0230259da73afe4302fbb2151b4d
Masieono <masonvoskamp@gmail.com>
2024-09-09 15:04:47 -0500

    beginning scattegories project

---
+++ C++/Practice_Projects/Scattergories/Scattergories.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+/*
+    Serves up a customizable game of scattegories
+    Customizable components -
+        - time limit
+        - number of topics
+        - banned categories (will exclude all topics within)
+        - preferred categories (will include a random number of topics between 1 and (num_topics / 2))
+        - repeated topics on/off 
+
+    Pulls from a separate file of categories / topics 
+    Remembers if a topic has been used within the session and does not repeat it
+
+
+
+
+*/
+
+
+
+using std::cout;
+using std::endl;
+
+int main () {
+
+    int number_of_topics;   // number of topics to get served in a round of play
+    int time_limit;         // number of seconds for a round of play
+
+
+
+
+
+
+    return 0;
+}
